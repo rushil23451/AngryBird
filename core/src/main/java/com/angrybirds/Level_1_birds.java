a76b17f5a8eaf4e3f51f432389044d24471e1b85
Arvind <141608792+BigDexx@users.noreply.github.com>
2024-10-27 17:03:18 +0530

    Add files via upload

--- core/src/main/java/com/angrybirds/Level_1_birds.java
+++ core/src/main/java/com/angrybirds/Level_1_birds.java
@@ -2,21 +2,19 @@ package com.angrybirds;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.ScreenUtils;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
-import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.physics.box2d.World;
-import com.badlogic.gdx.math.Vector2;
 
 public class Level_1_birds implements Screen {
     private Texture backgroundTexture;
@@ -29,6 +27,10 @@ public class Level_1_birds implements Screen {
     private Stage stage;
     private Texture pauseButtonTexture;
     private World world;
+    private ShapeRenderer shapeRenderer;
+    private float buttonRadius = 25f;
+    private float buttonX = 40f;
+    private float buttonY = 40f;
     private RedBird bird1;
     private YellowBird bird2;
     private Blackbird bird3;
@@ -36,11 +38,7 @@ public class Level_1_birds implements Screen {
     private WoodStructureHorizontal wood2;
     private WoodStructure wood3;
     private slingshot slingshot;
-
-
-
-
-    private Smallpig pig1;// Bird instance
+    private Smallpig pig1;
 
     public Level_1_birds(Main game) {
         this.game = game;
@@ -64,50 +62,26 @@ public class Level_1_birds implements Screen {
         camera.position.set(VIRTUAL_WIDTH / 2, VIRTUAL_HEIGHT / 2, 0);
         camera.update();
 
-        // Initialize Box2D world and Bird object
         world = new World(new Vector2(0, -9.8f), true);
-        slingshot=new slingshot(world,1.45f,1.5f,0.75f,1f);
+        slingshot = new slingshot(world, 1.45f, 1.5f, 0.75f, 1f);
         bird1 = new RedBird(world, 0.75f, 1.5f);
-        bird2= new YellowBird(world,1.5f,1.75f);
-        pig1= new Smallpig(world,6.35f,1.4f);
-        wood1= new WoodStructure(world,6.01f,1.21f,0.2f,0.6f);
-        wood2= new WoodStructureHorizontal(world,6.13f,1.7f,0.55f,0.2f);
-        wood3= new WoodStructure(world,6.55f,1.21f,0.2f,0.6f);
-
+        bird2 = new YellowBird(world, 1.5f, 1.75f);
+        pig1 = new Smallpig(world, 6.35f, 1.4f);
+        wood1 = new WoodStructure(world, 6.01f, 1.21f, 0.2f, 0.6f);
+        wood2 = new WoodStructureHorizontal(world, 6.13f, 1.7f, 0.55f, 0.2f);
+        wood3 = new WoodStructure(world, 6.55f, 1.21f, 0.2f, 0.6f);
 
         stage = new Stage(viewport);
         Gdx.input.setInputProcessor(stage);
 
         pauseButtonTexture = new Texture(Gdx.files.internal("pausebutton.png"));
-        ImageButton pauseButton = new ImageButton(new TextureRegionDrawable(pauseButtonTexture));
-        pauseButton.setPosition(VIRTUAL_WIDTH - 100, VIRTUAL_HEIGHT - 100);
-        pauseButton.setSize(100, 100);
-        pauseButton.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                System.out.println("Pause button clicked");
-                game.setScreen(new PauseScreen(game));
-            }
 
-            @Override
-            public void enter(InputEvent event, float x, float y, int pointer, Actor fromActor) {
-                pauseButton.setSize(115, 115);
-                pauseButton.setPosition(pauseButton.getX() - 7.5f, pauseButton.getY() - 7.5f);
-            }
-
-            @Override
-            public void exit(InputEvent event, float x, float y, int pointer, Actor toActor) {
-                pauseButton.setSize(100, 100);
-                pauseButton.setPosition(pauseButton.getX() + 7.5f, pauseButton.getY() + 7.5f);
-            }
-        });
-        stage.addActor(pauseButton);
+        shapeRenderer = new ShapeRenderer();
     }
 
     @Override
     public void render(float delta) {
         ScreenUtils.clear(Color.BLACK);
-
         world.step(1 / 60f, 6, 2);
 
         camera.update();
@@ -120,26 +94,41 @@ public class Level_1_birds implements Screen {
         Vector2 YellowbirdPosition = bird2.getPosition();
         Vector2 SmallpigPosition = pig1.getPosition();
 
-        spriteBatch.draw(bird1.getTexture(), RedbirdPosition.x * 100 - 25, RedbirdPosition.y * 100 - 25, 50 ,50);
+        spriteBatch.draw(bird1.getTexture(), RedbirdPosition.x * 100 - 25, RedbirdPosition.y * 100 - 25, 50, 50);
         spriteBatch.draw(bird2.getTexture(), YellowbirdPosition.x * 100 - 25, YellowbirdPosition.y * 100 - 25, 50, 50);
-        spriteBatch.draw(pig1.getTexture(), SmallpigPosition.x * 100 - 25, SmallpigPosition.y * 100 - 25,50, 50);
+        spriteBatch.draw(pig1.getTexture(), SmallpigPosition.x * 100 - 25, SmallpigPosition.y * 100 - 25, 50, 50);
 
         Vector2 wood1Position = wood1.getPosition();
         Vector2 wood2Position = wood2.getPosition();
         Vector2 wood3Position = wood3.getPosition();
         Vector2 slingshotPosition = slingshot.getPosition();
 
-
-        spriteBatch.draw(slingshot.getTexture(), slingshotPosition.x * 100 - (slingshot.getWidth() / 2), slingshotPosition.y * 100 - (slingshot.getHeight() / 2), slingshot.getWidth() * 100, slingshot.getHeight()* 100);
+        spriteBatch.draw(slingshot.getTexture(), slingshotPosition.x * 100 - (slingshot.getWidth() / 2), slingshotPosition.y * 100 - (slingshot.getHeight() / 2), slingshot.getWidth() * 100, slingshot.getHeight() * 100);
         spriteBatch.draw(wood1.getTexture(), wood1Position.x * 100 - (wood1.getWidth() / 2), wood1Position.y * 100 - (wood1.getHeight() / 2), wood1.getWidth() * 100, wood1.getHeight() * 100);
         spriteBatch.draw(wood2.getTexture(), wood2Position.x * 100 - (wood2.getWidth() / 2), wood2Position.y * 100 - (wood2.getHeight() / 2), wood2.getWidth() * 100, wood2.getHeight() * 100);
         spriteBatch.draw(wood3.getTexture(), wood3Position.x * 100 - (wood3.getWidth() / 2), wood3Position.y * 100 - (wood3.getHeight() / 2), wood3.getWidth() * 100, wood3.getHeight() * 100);
 
         spriteBatch.end();
 
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        shapeRenderer.setColor(Color.BLUE);
+        shapeRenderer.circle(buttonX, buttonY, buttonRadius);
+        shapeRenderer.end();
+
+        if (Gdx.input.isTouched()) {
+            float touchX = Gdx.input.getX();
+            float touchY = Gdx.graphics.getHeight() - Gdx.input.getY();
+
+            if (Math.sqrt((touchX - buttonX) * (touchX - buttonX) + (touchY - buttonY) * (touchY - buttonY)) <= buttonRadius) {
+                System.out.println("Win button clicked");
+                game.setScreen(new WinScreen(game));
+            }
+        }
+
         stage.act(delta);
         stage.draw();
     }
+
     @Override
     public void resize(int width, int height) {
         viewport.update(width, height);
@@ -158,6 +147,7 @@ public class Level_1_birds implements Screen {
         spriteBatch.dispose();
         stage.dispose();
         pauseButtonTexture.dispose();
+        shapeRenderer.dispose();
         world.dispose();
     }
 }
--- core/src/main/java/com/angrybirds/WinScreen.java
+++ core/src/main/java/com/angrybirds/WinScreen.java
@@ -4,50 +4,67 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.ScreenUtils;
 
 public class WinScreen implements Screen {
 
     private SpriteBatch batch;
     private Texture backgroundTexture;
     private Texture victoryTexture;
-    private TextureRegion victoryRegion;
+    private Stage stage;
+    private Main game;
 
-    public WinScreen() {
+    public WinScreen(Main game) {
+        this.game = game;
         batch = new SpriteBatch();
-
-        // Load the background image
-        backgroundTexture = new Texture(Gdx.files.internal("victory screen.jpg"));
-
-        // Load the victory icon and set its size to 150x150
-        victoryTexture = new Texture(Gdx.files.internal("victory.png"));
-        victoryRegion = new TextureRegion(victoryTexture, 0, 0, 150, 150);
     }
 
     @Override
     public void show() {
+        backgroundTexture = new Texture(Gdx.files.internal("victory screen.jpg"));
+        victoryTexture = new Texture(Gdx.files.internal("vctory.png"));
 
+        stage = new Stage();
+        Gdx.input.setInputProcessor(stage);
+
+
+        ImageButton victoryButton = new ImageButton(new TextureRegionDrawable(victoryTexture));
+        victoryButton.setSize(308 * 2, 91 * 2); // Adjust the size as needed
+
+
+        victoryButton.setPosition((Gdx.graphics.getWidth() - victoryButton.getWidth()) / 2,
+            (Gdx.graphics.getHeight() - victoryButton.getHeight()) / 2);
+
+        victoryButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                game.setScreen(new FirstScreen(game));
+            }
+        });
+
+        stage.addActor(victoryButton);
     }
 
     @Override
     public void render(float delta) {
+        ScreenUtils.clear(0, 0, 0, 1);
 
         batch.begin();
-
-
         batch.draw(backgroundTexture, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-
-
-        float xPosition = (Gdx.graphics.getWidth() - 150) / 2;
-        float yPosition = Gdx.graphics.getHeight() - 100 - 150;
-        batch.draw(victoryRegion, xPosition, yPosition, 150, 150);
-
         batch.end();
+
+        stage.act(delta);
+        stage.draw();
     }
 
     @Override
     public void resize(int width, int height) {
-
+        stage.getViewport().update(width, height, true);
     }
 
     @Override
@@ -57,13 +74,15 @@ public class WinScreen implements Screen {
     public void resume() {}
 
     @Override
-    public void hide() {}
+    public void hide() {
+        dispose();
+    }
 
     @Override
     public void dispose() {
-
         batch.dispose();
-        backgroundTexture.dispose();
-        victoryTexture.dispose();
+        if (backgroundTexture != null) backgroundTexture.dispose();
+        if (victoryTexture != null) victoryTexture.dispose();
+        stage.dispose();
     }
 }
